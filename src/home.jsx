83b469095495e9a69b2506f63e7a89ab7a1ec0c1
shyam vaya <vayashyam550@gmail.com>
2025-02-20 10:47:52 +0530

    changes

--- src/home.jsx
+++ src/home.jsx
@@ -10,7 +10,8 @@ import { FaNodeJs } from "react-icons/fa6";
 import { ReactTyped, Typed } from "react-typed";
 function Home() {
   return (
-    <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20 bg-white/50 backdrop-blur-sm rounded-lg p-8 shadow-xl">
+    <div name="Home" className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20 bg-white/50 backdrop-blur-sm rounded-lg p-8 shadow-xl">
+
       <div className="flex flex-col md:flex-row">
         <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
           <span className="text-xl">Welcome In My Feed</span>
